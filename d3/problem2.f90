0405241a73c936db5d28641af901e5d0979b5bdf
florestan-fl <flo.fargetton@gmail.com>
2023-12-04 18:46:16 +0100

    feat: d3 - problem2 (and improve problem1)

--- d3/problem2.f90
+++ d3/problem2.f90
@@ -29,8 +29,8 @@ subroutine problem()
     logical :: eof
     integer, parameter :: nblines=140
     integer, dimension(nblines, 140) :: array
-    integer :: linewidth, last_added
-
+    integer :: linewidth
+    integer :: fst_adj, scd_adj, nb_adj
     array(:,:) = 0
     res = 0
     open(UNIT=1, file=input, status='old')
@@ -64,6 +64,10 @@ readl:    do while(.not. eof)
                     array(j, k) = full_temp
                 end do
                 streak = streak + 1
+            else if (s(i:i) == '*') then
+                full_temp = 0
+                array(j, i) = -2
+                streak = 0
             else
                 full_temp = 0
                 array(j, i) = -1
@@ -73,26 +77,40 @@ readl:    do while(.not. eof)
         j = j + 1
     end do readl
 
-    last_added = 0
+    fst_adj = 0
+    scd_adj = 0
+    nb_adj = 0
     do e=1, linewidth
         do f=1, nblines
-            if (array(e,f)>0) then
-                if (array(e,f) == last_added) then
-                    cycle
-                end if
+            write(*, '(I3)', advance='no') array(e,f)
+        end do
+        print *, ''
+    end do
+    do e=1, linewidth
+        do f=1, nblines
+            if (array(e,f) == -2) then
+                nb_adj = 0
+                fst_adj = 0
+                scd_adj = 0
                 do i=max(0, e-1),min(nblines, e+1)
                     do j=max(0, f-1), min(linewidth, f+1)
                         if (i==e .and. j == f) then
                             cycle
                         end if
-                        if (array(i,j) < 0) then
-                            res = res + array(e, f)
-                            last_added = array(e,f)
+                        if (array(i,j) > 0 .and. array(i,j) /= fst_adj .and. array(i,j) /= scd_adj) then
+                            nb_adj = nb_adj + 1
+                            if (fst_adj == 0) then
+                                fst_adj = array(i,j)
+                            else if (scd_adj == 0) then
+                                scd_adj = array(i,j)
+                            end if
                         end if
+                        
                     end do
                 end do
-            else
-                last_added = 0
+                if (nb_adj == 2) then
+                    res = res + fst_adj * scd_adj
+                end if
             end if
         end do
     end do
